78deecea4eaf6e3f7b61846b7ae5d2e3e62ecf87
zuvvil <zuvvilgautam007@gmail.com>
2023-07-16 12:38:31 +0530

    connect database for todo list

--- controller/homecontroller.js
+++ controller/homecontroller.js
@@ -1,5 +1,20 @@
+
+
+const TodoLists = require('../models/todo_list');
+
+//function for redirecting to main homepage
 module.exports.home = function(req,res){
-    res.render('home', {
-        title:'ToDo app',
+
+    TodoLists.find(req.body)
+    .then(todo => {
+        console.log("Todo list fetched successfully");
+        res.render('home', {
+        title: 'ToDo app',
+        todoList:todo
+        });
+    })
+    .catch(err =>{
+        console.log("Error in retrieving todo list");
+        return;
     });
 } 
